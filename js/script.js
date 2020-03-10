ae28a4c630531c1ad29643103437b398875a1da2
ccpatents <ccpatents@github.com>
2020-03-11 02:17:54 +0900

    .

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
         navigationPosition: 'right',
         navigationTooltips: ['검색량이 너무 많죠?', '검색 범위를 쉽게 조절할 수 있습니다.', '단어에만 신경쓰면 됩니다.', '이 외에도 다양한 기능들이 있어요.', '마이크로소프트 스토어에서 안전하게'],
         showActiveTooltip: true,
-        verticalCentered: false
+        verticalCentered: false,
     });
     /* end dot nav */
 })(jQuery); // End of use strict
