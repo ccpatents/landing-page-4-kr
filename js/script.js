65dd9ebd703e88c2857db8095eb2c26567adaaa1
ccpatents <ccpatents@github.com>
2020-03-10 15:05:21 +0900

    .

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 (function ($) {
-    $('#fullpage').fullpage({
+    /*$('#fullpage').fullpage({
         anchors: ['firstPage', 'secondPage'],
         sectionsColor: ['transparent', '#7E8F7C'],
         navigation: true,
         navigationPosition: 'right',
         navigationTooltips: ['특허 검색 너무 어렵죠?', '우리에겐 CCPatents가 있습니다.'],
         showActiveTooltip: true,
-        responsiveWidth: 768,
         verticalCentered: false
-    });
+    });*/
     /* end dot nav */
 })(jQuery); // End of use strict
