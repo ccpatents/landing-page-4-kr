b7a9e24f94327fbe6557986da0a3c217440a4aad
joondong2 <kwonjoondong@gmail.com>
2020-03-11 16:57:49 +0900

    .

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ var current_section = 1;
         }
     }
 
-    let filter = "win32|win64|mac";
+    let filter = "win16|win32|win64|mac";
     if (navigator.platform) {
         if (filter.indexOf(navigator.platform.toLowerCase()) < 0) {
             is_mobile = true;
@@ -50,13 +50,14 @@ var current_section = 1;
     }
 
     $(window).resize(function () {
+        console.log('hello');
         if (is_mobile && !resize_timer) {
             resize_timer = setTimeout(function () {
               resize_timer = null;
               let width = window.innerWidth;
               let height = window.innerHeight;
 
-              if((width < 576) && (height > (width+width))) {
+              if((width < 576) && (height > (width * 1.6))) {
                 $('mobile_app').show();
               } else {
                 $('mobile_app').hide();
